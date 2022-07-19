6d2da0574bd2de09c4711f108c776614c49a13e6
Artem Zhikharev <artemziharew@gmail.com>
2022-07-19 20:55:46 +0300

    Добавлена проверка авторизации в контроллерах

--- src/main/kotlin/com/trialbot/tasktest/controllers/AuthController.kt
+++ src/main/kotlin/com/trialbot/tasktest/controllers/AuthController.kt
@@ -6,6 +6,7 @@ import com.trialbot.tasktest.models.UserRegisterRequest
 import com.trialbot.tasktest.services.UserService
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = ["*"], maxAge = 86400)
 class AuthController(
     private val userService: UserService,
     private val authenticationProvider: AuthenticationProvider
--- src/main/kotlin/com/trialbot/tasktest/controllers/Home.kt
+++ src/main/kotlin/com/trialbot/tasktest/controllers/Home.kt
@@ -6,6 +6,7 @@ import com.trialbot.tasktest.models.toDto
 import com.trialbot.tasktest.repositories.UserRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,6 +15,7 @@ data class SomeObject(val name: String, val index: Int)
 
 @RestController
 @RequestMapping("/api/test")
+@CrossOrigin(origins = ["*"], maxAge = 86400)
 class Home(private val userRepository: UserRepository) {
 
     @GetMapping("/")
